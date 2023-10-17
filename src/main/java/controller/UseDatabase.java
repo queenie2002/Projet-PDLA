bd52dc7bea745525f816f8817e26a6f4dedd4f48
queenie2002 <101265104+queenie2002@users.noreply.github.com>
2023-10-17 16:30:41 +0200

    Update UseDatabase.java

--- src/main/java/controller/UseDatabase.java
+++ src/main/java/controller/UseDatabase.java
@@ -1,8 +1,24 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class UseDatabase {
 
-
+  public Connection connectToDatabase () {
+    Connection conn = null;
+    while (conn = null) {
+      try {
+        String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/";
+        String username = "projet_gei_002";
+        String password = "Lio0Uhah";
+        conn = DriverManager.getConnection(url, username, password);
+      } catch (Exception e) {
+        System.out.println("erreur on n'arrive pas à se connecter dans connectToDabatase");
+        e.printStackTrace();
+      }
+    }
+    return conn;
 }
 
