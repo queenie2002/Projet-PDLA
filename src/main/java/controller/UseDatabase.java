d55220377d5a0a4989c0f392e32f3e1fd3675f7a
queenie2002 <101265104+queenie2002@users.noreply.github.com>
2023-10-17 16:54:34 +0200

    Update UseDatabase.java

--- src/main/java/controller/UseDatabase.java
+++ src/main/java/controller/UseDatabase.java
@@ -14,6 +14,14 @@ public class UseDatabase {
         String username = "projet_gei_002";
         String password = "Lio0Uhah";
         conn = DriverManager.getConnection(url, username, password);
+        System.out.println("on s'est connecté");
+
+
+        
+        Statement stmt = conn.createStatement();
+        String connectSQL = "use projet_gei_002; "; 
+        stmt.execute(connectSQL);
+        
       } catch (Exception e) {
         System.out.println("erreur on n'arrive pas à se connecter dans connectToDabatase");
         e.printStackTrace();
@@ -21,4 +29,32 @@ public class UseDatabase {
     }
     return conn;
 }
+
+public void doStatementDatabase (Connection conn, String toDo) {
+  try {
+    Statement stmt = conn.createStatement();
+    stmt.execute(toDo);
+  } catch (Exception e) {
+      System.out.println("pas réussi à faire le SQL dans doStatementDatabase");
+      e.printStackTrace();
+  }
+
+  
+}
+  
+
+  
+public void disconnectToDatabase (Connection conn) {
+  try {
+    if (conn != null) {
+        conn.close();
+        System.out.println("on s'est déconnecté");
+  }
+  } catch (Exception e) {
+      System.out.println("erreur on n'arrive pas à se déconnecter de disconnectToDatabase");
+      e.printStackTrace();
+  }  
+}
+
+  
 
