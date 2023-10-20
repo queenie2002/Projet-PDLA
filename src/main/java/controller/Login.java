e7160a9fa7dda9d6935c79cc05ae3b08fa9221d9
queenie2002 <101265104+queenie2002@users.noreply.github.com>
2023-10-20 17:38:09 +0200

    Update Login.java

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -5,6 +5,13 @@ public class Login {
 
     private static void createGUI () {
         //create and set up window
+        JFrame frame = new JFrame("Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("Sign Up");
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.START);
+        
     }
 
 }
