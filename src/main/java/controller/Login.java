d626dd76a59a3a6e8a46b28018caf91d1bd4a562
“Queenie2002” <yqnguyen@insa-toulouse.fr>
2023-10-20 18:49:50 +0200

    we're hopeful

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -1,17 +1,61 @@
 package controller;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Login {
 
-    private static void createGUI () {
+    private static void createAndShowGUI () {
+        JTextField[] jtFields = new JTextField[3];
+        jtFields[0] = new JTextField();
+        jtFields[1] = new JTextField();
+
+        JButton button_login = new JButton("login");
+        button_login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                /*get text and verify that the password corresponds to the username*/
+                String username = jtFields[0].getText();
+                String password = jtFields[1].getText();
+                System.out.println(username);
+                System.out.println(password);
+
+            }
+        });
+
         //create and set up window
         JFrame frame = new JFrame("Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel emptyLabel = new JLabel("Sign Up");
+        JLabel emptyLabel = new JLabel("Login", JLabel.CENTER);
         emptyLabel.setPreferredSize(new Dimension(175, 100));
-        frame.getContentPane().add(emptyLabel, BorderLayout.START);
+        frame.getContentPane().add(emptyLabel, BorderLayout.PAGE_START);
+
+        JPanel p = new JPanel(new GridLayout(1,2));
+        p.add(new JLabel("Username"));
+        p.add(jtFields[0]);
+        p.add(new JLabel("Password"));
+        p.add(jtFields[1]);
+
+        p.add(button_login, p);
+        frame.add(p);
+
+
+        // make window's dimension fit its content
+        frame.pack();
+        // Display the window.
+        frame.setVisible(true);
         
+    }
+
+    public static void main(String[] args) {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 
 }
--- src/main/java/run/ActualApp.java
+++ src/main/java/run/ActualApp.java
@@ -5,7 +5,7 @@ import model.*;
 import java.sql.*;
 
 public class ActualApp  {
-    static String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/";
+    static String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/projet_gei_002";
     static String username = "projet_gei_002";
     static String password = "Lio0Uhah";
 
@@ -18,20 +18,21 @@ public class ActualApp  {
          *               INITIALIZING
         ***********************************************/
 
-        Connection connection = null;
+        /*Connection connection = null;
         try {
             connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
+            System.out.println(InitializeDatabase.createTableUser());
             statement.execute(InitializeDatabase.createTableUser());
-            statement.execute(InitializeDatabase.createTableVolunteer());
-            statement.execute(InitializeDatabase.createTablePatient());
-            statement.execute(InitializeDatabase.createTableGuarantor());
-            statement.execute(InitializeDatabase.createTableService());
+          //  statement.execute(InitializeDatabase.createTableVolunteer());
+          //  statement.execute(InitializeDatabase.createTablePatient());
+           // statement.execute(InitializeDatabase.createTableGuarantor());
+          //  statement.execute(InitializeDatabase.createTableService());
         } catch (SQLException e) {
             System.out.println("problem");
-        }
+        }*/
 
-        /*
+
         UseDatabase database = new UseDatabase();
         Connection conn;
         conn = database.connectToDatabase();
@@ -41,7 +42,7 @@ public class ActualApp  {
         database.doStatementDatabase(conn,InitializeDatabase.createTablePatient());
         database.doStatementDatabase(conn,InitializeDatabase.createTableGuarantor());
         database.doStatementDatabase(conn,InitializeDatabase.createTableService());
-        database.disconnectToDatabase(conn);*/
+        database.disconnectToDatabase(conn);
 
         /**********************************************
          *            STARTING PROGRAM
