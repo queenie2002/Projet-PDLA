ae111dae9df0633b0241fe50dc4e3297450c6dc7
“Queenie2002” <yqnguyen@insa-toulouse.fr>
2023-11-05 15:47:15 +0100

    MissionTabVolunteer

---
+++ src/main/java/view/MissionTabVolunteer.java
@@ -0,0 +1,45 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MissionTabVolunteer {
+    private static void createAndShowGUI () {
+        JFrame frame = new JFrame("Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("Login", JLabel.CENTER);
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.PAGE_START);
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JButton button1 = new JButton("Show past missions");
+        JButton button2 = new JButton("Add new mission");
+        JButton button3 = new JButton("Available missions");
+
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+
+        frame.getContentPane().setLayout(null); // Manually position the panel
+
+        // Set the panel's position and size
+        panel.setBounds(100, 100, 200, 100);
+
+        frame.add(panel);
+
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null); // Center the frame on the screen
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
