ebc22afd7d772b55517fb8c7d9a716bb55235195
“Queenie2002” <yqnguyen@insa-toulouse.fr>
2023-11-13 11:05:04 +0100

    initializing database

--- src/main/java/run/ActualApp.java
+++ src/main/java/run/ActualApp.java
@@ -21,23 +21,9 @@ public class ActualApp  {
     public static void main(String[] args){
 
         /**********************************************
-         *               INITIALIZING
+                        INITIALIZING
         ***********************************************/
 
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-            statement.execute(InitializeDatabase.createTableUser());
-            statement.execute(InitializeDatabase.createTableVolunteer());
-            statement.execute(InitializeDatabase.createTablePatient());
-            statement.execute(InitializeDatabase.createTableGuarantor());
-            statement.execute(InitializeDatabase.createTableService());
-        } catch (SQLException e) {
-            System.out.println("problem");
-        }
-
-        /*
         UseDatabase database = new UseDatabase();
         Connection conn;
         conn = database.connectToDatabase();
@@ -47,10 +33,10 @@ public class ActualApp  {
         database.doStatementDatabase(conn,InitializeDatabase.createTablePatient());
         database.doStatementDatabase(conn,InitializeDatabase.createTableGuarantor());
         database.doStatementDatabase(conn,InitializeDatabase.createTableService());
-        database.disconnectToDatabase(conn);*/
+        database.disconnectToDatabase(conn);
 
         /**********************************************
-         *            STARTING PROGRAM
+                     STARTING PROGRAM
          ***********************************************/
         //Login login = new Login();
 
