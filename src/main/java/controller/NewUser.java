bb36fe2e4b0b5ae7e56ac962604c27755ced138b
georgia <koutsodi@insa-toulouse.fr>
2023-11-15 19:05:01 +0100

    newservice newuser usedatabase

--- src/main/java/controller/NewUser.java
+++ src/main/java/controller/NewUser.java
@@ -1,7 +1,6 @@
 package controller;
 
-import model.Service;
-import model.User;
+import model.*;
 import java.sql.*;
 
 
--- src/main/java/controller/UseDatabase.java
+++ src/main/java/controller/UseDatabase.java
@@ -1,9 +1,6 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UseDatabase {
 
