668f7ca9bfbfd674ea37b41518930e6ec4005adb
georgia <koutsodi@insa-toulouse.fr>
2023-11-15 19:25:28 +0100

    ShowAvailableMissions pas fini

--- src/main/java/view/ShowAvailableMissions.java
+++ src/main/java/view/ShowAvailableMissions.java
@@ -1,6 +1,26 @@
 package view;
 
+import controller.UseDatabase;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class ShowAvailableMissions {
+
+    public ShowAvailableMissions(int idUser) {
+        /*se connecter a la database et recupérer tous les services qui ont status=0*/
+        /*connection*/
+        UseDatabase database = new UseDatabase();
+        Connection conn;
+        conn = database.connectToDatabase();
+
+        String findType = "select * from service where status=0";
+        ResultSet res = database.doQueryDatabase(conn, findType);
+
+        /*show available missions*/
+
+
+    }
 
 
 
