620c6de18e2d7eda078e7fea98a0023355fb65d3
georgia <koutsodi@insa-toulouse.fr>
2023-11-21 17:17:50 +0100

    test new patient

--- src/test/java/controller/NewUserTest.java
+++ src/test/java/controller/NewUserTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NewUserTest {
 
     @Test
-    public void testNewUser() throws SQLException {
+    public void testNewUserPatient() throws SQLException {
 
         //we initialize de the database
         Initialize();
@@ -74,6 +74,53 @@ public class NewUserTest {
         assertEquals(dateOfBirth, dateOfBirthDatabase, "should be equal 2002-12-12");
     }
 
+    @Test
+    public void testNewUserVolunteer() throws SQLException {
 
+        //we initialize de the database
+        Initialize();
+
+        //we create a user
+        String firstName = "yquynh";
+        String lastName = "nguyen";
+        int type = 1;
+        String password = "pwd";
+        String dateOfBirth = "2002-12-12";
+
+        model.User user = new User(firstName,lastName,type,password,dateOfBirth);
+        controller.NewUser.AddNewUser(user);
+        int idUser = user.getId();
+
+
+        //connection
+        UseDatabase database = new UseDatabase();
+        Connection conn;
+        conn = database.connectToDatabase();
+
+        //we get the user with our id
+        String findType = "select * from user where idUser = " + idUser + " ;";
+        ResultSet res = database.doQueryDatabase(conn, findType);
+
+        //we get all the type
+        int typeDatabase = -1;
+
+
+        while (true) {
+            try {
+                if (!res.next()) break;
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            try {
+                typeDatabase = res.getInt("type");
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+        //we verify that the type is equal to 1 (which is the type of  volunteer)
+        assertEquals(type, typeDatabase, "should be equal 1");
+
+    }
 
 }
